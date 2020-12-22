d0150cd2c1c29cf98905b3de812cefed3cb949ed
Caleb Denio <cjdenio44@gmail.com>
2020-12-22 11:30:52 -0500

    logging

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-const http = require("http")
-const express = require("express")
-const ws = require("ws")
+const http = require("http");
+const express = require("express");
+const ws = require("ws");
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-const wss = new ws.Server({ server })
+const wss = new ws.Server({ server });
 
-app.use(express.json())
+wss.on("connection", (sock) => {
+  console.log("connection");
+
+  sock.on("message", (msg) => {
+    console.log(`Received: ${msg}`);
+  });
+
+  sock.on("close", () => {
+    console.log("disconnect");
+  });
+});
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("howdy")
-})
+  res.send("howdy");
+});
 
 app.post("/webhook", (req, res) => {
   res.end();
 
   try {
-    const {name, username} = req.body
+    const { name, username } = req.body;
 
-    wss.clients.forEach(client => {
-      client.send(`@${name} posted to their scrapbook! scrapbook.hackclub.com/${username}`)
-    })
-  } catch(e) {
-    console.log(e.message)
+    wss.clients.forEach((client) => {
+      client.send(
+        `@${name} posted to their scrapbook! scrapbook.hackclub.com/${username}`,
+      );
+    });
+  } catch (e) {
+    console.log(e.message);
   }
-})
+});
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("app started")
-})
+  console.log("app started");
+});
